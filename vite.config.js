4775ae56341a5f4bfd7c72dfd3de879751a877d9
Samuel Nikhil <samuelnikhil@example.com>
2026-04-25 07:29:24 -0700

    Final local-only configuration and seeding fix

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/_/backend': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/_\/backend/, '')
       }
     }
   }
